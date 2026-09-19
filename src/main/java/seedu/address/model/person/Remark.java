b15eb5460250fb827ed99905be2d98232f35ad09
Hoong Ken <hhken1129@gmail.com>
2026-09-19 12:03:50 +0800

    Fix remark command CI failures

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,11 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Creates a {@code Remark}.
+     *
+     * @param remark the remark value
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
